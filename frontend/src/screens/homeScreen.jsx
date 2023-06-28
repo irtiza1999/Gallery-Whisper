9e237d9c53703bcc3f8d80244fedfa201a5cda6c
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2023-06-28 15:22:07 +0600

    membership card design done

--- frontend/src/screens/homeScreen.jsx
+++ frontend/src/screens/homeScreen.jsx
@@ -1,10 +1,12 @@
 import Hero from '../components/hero'
 import AllProducts from '../components/AllProducts'
+import MembershipCards from '../components/MembershipCards'
 const homeScreen = () => {
   return (
     <>
       <Hero />
       <AllProducts />
+      <MembershipCards />
     </>
   )
 }
